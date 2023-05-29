5853b456de3fd5a4b79241fdff2ca56a9404644f
Md Wahiduzzaman Emon <wahedemon09@gmail.com>
2023-05-29 13:55:44 +0600

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <WebView 
+    javaScriptEnabled={true}
+    originWhitelist={['*']}
+   source={{ uri: 'http://192.168.0.103:5006' }} style={{ marginTop: 20 }} />
   );
 }
 
